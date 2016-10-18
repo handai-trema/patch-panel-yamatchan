474239dd116e4b18f6b1a2a0ff3cc6be97d16074
yamatchan <u.ma.ymd@gmail.com>
2016-10-19 07:15:34 +0900

    add port-mirroring and list

--- lib/patch_panel.rb
+++ lib/patch_panel.rb
@@ -2,10 +2,12 @@
 class PatchPanel < Trema::Controller
   def start(_args)
     @patch = Hash.new { [] }
+    @mirror = Hash.new { |h,k| h[k] = {} }
     logger.info 'PatchPanel started.'
   end
 
   def switch_ready(dpid)
+    @patch[dpid] = []
     @patch[dpid].each do |port_a, port_b|
       delete_flow_entries dpid, port_a, port_b
       add_flow_entries dpid, port_a, port_b
@@ -14,12 +16,35 @@ class PatchPanel < Trema::Controller
 
   def create_patch(dpid, port_a, port_b)
     add_flow_entries dpid, port_a, port_b
-    @patch[dpid] += [port_a, port_b].sort
-  end
+    pair = port_a < port_b ? [port_a, port_b] : [port_b, port_a]
+    @patch[dpid] << pair
+   end
 
   def delete_patch(dpid, port_a, port_b)
     delete_flow_entries dpid, port_a, port_b
     @patch[dpid] -= [port_a, port_b].sort
+  end
+
+  def add_mirror(dpid, port_a, port_b)
+    logger.info "#add_mirror"
+    add_mirror_entries dpid, port_a, port_b
+  end
+
+  def list()
+    str = ""
+
+    @patch.each do |dpid, pairs|
+      str += "#dpid = 0x#{dpid.to_s(16)}\n"
+      pairs.each do |pair|
+        str += "* #{pair[0]} <-> #{pair[1]}\n"
+      end
+      @mirror[dpid].each do |port_a, port_m|
+        str += "+ #{port_a} --> #{port_m}\n"
+      end
+      str += "\n"
+    end
+
+    str
   end
 
   private
@@ -36,5 +61,30 @@ class PatchPanel < Trema::Controller
   def delete_flow_entries(dpid, port_a, port_b)
     send_flow_mod_delete(dpid, match: Match.new(in_port: port_a))
     send_flow_mod_delete(dpid, match: Match.new(in_port: port_b))
+  end
+
+  def add_mirror_entries(dpid, port_a, port_m)
+    port_b = nil
+
+    @patch[dpid].each do |pair|
+      port_b = pair[1] if pair[0] == port_a
+      port_b = pair[0] if pair[1] == port_a
+    end
+
+    return if port_b.nil?
+
+    send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port_a),
+                      actions: [
+                        SendOutPort.new(port_b),
+                        SendOutPort.new(port_m),
+                      ])
+    send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port_b),
+                      actions: [
+                        SendOutPort.new(port_a),
+                        SendOutPort.new(port_m),
+                       ])
+    @mirror[dpid][port_a] = port_m
   end
 end
